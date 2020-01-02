b86931106dd6e4c4b47ab27d410a5b116b5b982c
Darren Jones <djaychela@gmail.com>
2020-01-02 21:33:30 +0000

    display improvements, soundtracks added

--- apc_display.py
+++ apc_display.py
@@ -28,11 +28,12 @@ pygame.mixer.init(frequency=44100)
 pygame.mixer.set_num_channels(99)
 
 # font creation
+print("Initialising fonts...")
 tinyArial = pygame.font.SysFont("arial", 15)
 smallArial = pygame.font.SysFont("arial", 25)
 medArial = pygame.font.SysFont("arial", 40)
 largeArial = pygame.font.SysFont("arial", 65)
-
+print("Fonts initialized!")
 
 # button class for midi io and audio playback
 class button:
@@ -44,7 +45,7 @@ class button:
             self.state = False
         self.audio_file = audio_file
         self.channel = self.note_number % 8
-        self.friendly_name = audio_file.split('/')[-1][:-4]
+        self.friendly_name = audio_file.split("/")[-1][:-4]
 
     def set_state(self, state):
         if isinstance(state, bool):
@@ -82,6 +83,53 @@ class button:
                 pass
 
 
+class playlist_button:
+    def __init__(self, note_number, audio_file, state=False):
+        self.note_number = note_number
+        if isinstance(state, bool):
+            self.state = state
+        else:
+            self.state = False
+        self.audio_file = audio_file
+        self.channel = self.note_number % 8
+        self.friendly_name = audio_file.split("/")[-1][:-4]
+
+    def set_state(self, state):
+        if isinstance(state, bool):
+            self.state = state
+            self.midi_output()
+            self.audio_output()
+
+    def toggle_state(self):
+        self.state = not self.state
+        self.midi_output()
+        self.audio_output()
+
+    def get_state(self):
+        return self.state
+
+    def output_state(self):
+        pass
+
+    def midi_output(self):
+        if self.state:
+            velocity = 127
+        else:
+            velocity = 0
+        out_msg = mido.Message("note_on", note=self.note_number, velocity=velocity)
+        outport.send(out_msg)
+
+    def audio_output(self):
+        if self.state:
+            self.audio = pygame.mixer.music.load(self.audio_file)
+            pygame.mixer.music.play()
+        else:
+            try:
+                pygame.mixer.music.fadeout(1000)
+            except:
+                pass
+
+
 # import files from json config file #
 config_path = os.path.join(os.getcwd(), "config", config_file)
 with open(config_path, "r") as f:
@@ -101,10 +149,12 @@ buttons = []
 for idx in range(63):
     file_path = os.path.join(os.getcwd(), "audio", samples_list[idx])
     buttons.append(button(idx, file_path, False))
-for idx in range(63, 99):
+for idx in range(63, 82):
     file_path = os.path.join(os.getcwd(), "audio", "dummy.wav")
     buttons.append(button(idx, file_path, False))
-
+for idx in range(82, 90):
+    file_path = os.path.join(os.getcwd(), "playlists", "underscore.mp3")
+    buttons.append(playlist_button(idx, file_path, False))
 
 channels = [0 for _ in range(9)]
 prev_state = [0 for _ in range(99)]
@@ -116,13 +166,16 @@ while True:
     for msg in inport.iter_pending():
         # perform actions depending on message type
         if msg.type == "note_on":
-            buttons[msg.note].toggle_state()
+            # TODO: correct button number filtering
+            if msg.note <= 63 or msg.note >= 82:
+                buttons[msg.note].toggle_state()
 
         elif msg.type == "control_change":
             channels[msg.control - 48] = msg.value
 
     # display update loop
     for idx in range(99):
+        # TODO: doesn't work for soundtrack buttons
         current_state[idx] = pygame.mixer.Channel(idx).get_busy()
         if current_state[idx] != prev_state[idx]:
             if current_state[idx] == 0:
@@ -133,8 +186,6 @@ while True:
         if event.type == pygame.QUIT:
             quit()
 
-    # time.sleep(0.5)
-
     # screen display
     screen.fill(BLACK)
 
@@ -142,18 +193,43 @@ while True:
     # current sample set
     # widgets for playback channels
     # widget for mp3 playback
-    
+    # TODO: soundtrack buttons mutually exclusive
+    # TODO: selection of grid buttons from keyboard
+    # TODO: display of current sound set on screen
+
     # pad display
     for idx, c in enumerate(buttons[:64]):
-        
+
         x_loc = 20 + ((idx % 8) * (button_size + 10))
         y_loc = 20 + (button_size + 10) * 7 - ((idx // 8) * (button_size + 10))
         pygame.draw.rect(screen, WHITE, (x_loc, y_loc, button_size, button_size))
         if current_state[idx]:
-            pygame.draw.rect(screen, GREY, (x_loc + 2, y_loc + 2, button_size - 4, button_size - 4))
+            pygame.draw.rect(
+                screen, GREY, (x_loc + 2, y_loc + 2, button_size - 4, button_size - 4)
+            )
         else:
-            pygame.draw.rect(screen, BLACK, (x_loc + 2, y_loc + 2, button_size - 4, button_size - 4))
+            pygame.draw.rect(
+                screen, BLACK, (x_loc + 2, y_loc + 2, button_size - 4, button_size - 4)
+            )
         text = tinyArial.render(buttons[idx].friendly_name, True, WHITE)
+        screen.blit(text, (x_loc + 3, y_loc + 3))
+
+    # playlist button display
+    for idx, c in enumerate(buttons[82:90]):
+
+        x_loc = 20 + (8 * (button_size + 10))
+        y_loc = 20 + (idx * (button_size + 10))
+        pygame.draw.rect(screen, WHITE, (x_loc, y_loc, button_size, button_size))
+        # TODO: this bit doesn't work!
+        if current_state[idx + 82]:
+            pygame.draw.rect(
+                screen, GREY, (x_loc + 2, y_loc + 2, button_size - 4, button_size - 4)
+            )
+        else:
+            pygame.draw.rect(
+                screen, BLACK, (x_loc + 2, y_loc + 2, button_size - 4, button_size - 4)
+            )
+        text = tinyArial.render(buttons[idx + 82].friendly_name, True, WHITE)
         screen.blit(text, (x_loc + 3, y_loc + 3))
 
     # volume slider code
@@ -162,15 +238,9 @@ while True:
         y_loc = 680
         pygame.draw.rect(screen, WHITE, (x_loc, y_loc, button_size, 100))
         pygame.draw.rect(screen, BLACK, (x_loc + 2, y_loc + 2, button_size - 4, 96))
-        pygame.draw.rect(screen, GREY, (x_loc + 2, y_loc + 78 - int(c/1.67) ,button_size - 4,20))
-
-        
-        
-    
-    
-
-    # smallArial.render_to(screen, (100 + r.width, 580), " HORIZONTAL",
-    #                pygame.Color('blue'),None, size=(48, 24))
+        pygame.draw.rect(
+            screen, GREY, (x_loc + 2, y_loc + 78 - int(c / 1.67), button_size - 4, 20)
+        )
 
     time.sleep(0.05)
 
